fd0c0780425ac7f5f7abc98279769e7c035594a6
Zhully <agunregezhuliakho@gmail.com>
2024-09-12 15:49:39 -0700

    starter file

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import { Routes, Route } from 'react-router-dom'
+import Homepage from './pages/Homepage/Homepage'
+import Cart from './Components/Cart/Cart'
+import ProductDetails from './pages/ProductDetails/ProductDetails'
+import Footer from './Components/Footer/Footer'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Homepage />}/>
+        <Route path='/cart' element={<Cart />}/>
+        <Route path='/product/:id' element={<ProductDetails />} />
+      </Routes>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
